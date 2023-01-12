0d079caf0fcf2fb0e91af7ce4691ffb29772cf43
Kairat <burambaevkairat@mail.ru>
2023-01-12 15:08:50 +0300

    newbranch variant2 added

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 import requests
 import json
+import pytz
+from datetime import datetime
+
+def print_datetime():
+    week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
+    tz = pytz.timezone('Asia/Yekaterinburg')
+    day = datetime.now(tz)
+    week_number = datetime.today().isocalendar()[1]
+    today = week[day.weekday()]
+    if week_number % 2:
+        flag = 1
+        print(f'Сегодня {today}   чётной недели')
+    else:
+        flag = 2
+        print(f'Сегодня {today} нечётной недели')
+    return today, flag
 
 def group_name():
-    name = str(input('Введите Имя: '))
+    name = str(input('Выберите Имя: '))
     s = requests.get(f'https://timetable.magtu.ru/api/v2/search?q={name}')
     data = s.json()
     if data == []:
@@ -11,52 +27,47 @@ def group_name():
     else:
         return data 
 
-def print_result(data):
-    messege = 'Результаты поиска:\n'
-    if flag == 1:
-        for elem in data:
-            if elem['url'][-1] not in 'АБВГДАЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ':
-                messege += elem['url'] + ' id: ' + str(elem['id']) + '\n'
-    elif flag == 2:
-        for elem in data:
-            if elem['url'][-1] in 'АБВГДАЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ':
-                messege += elem['url'] + ' id: ' + str(elem['id']) + '\n'
-    return messege
-
-flag = int(input('1 - Поиск группы, 2 - Поиск преподавателя: '))
-
-print(print_result(group_name()))
-
-def schedule():
-    id = int(input('Введите id: '))
-    if flag == 1:
-        s = requests.get(f'https://timetable.magtu.ru/api/v2/groups/{id}/schedule')
-        data = s.json()
-    elif flag == 2:
-        s = requests.get(f'https://timetable.magtu.ru/api/v2/teachers/{id}/schedule')
-        data = s.json()
-    return data
-
 def nice_print(data):
-    messege = data['url'] + '\n' + 'Неделя: ' + data['schedule'][0]['week'] + '\n'
+    messege = '\n' + data['url'] + '\n' + 'Неделя: ' + data['schedule'][0]['week'] + '\n'
     for elem in data['schedule'][0]['days']:
-        messege += elem['day'] + '\n'
+        messege += '\n' + elem['day'] + '\n'
         for i in elem['events']:
             if elem['events'].count(i) > 0:
-                messege += str(i['event_index']) + ' ' + i['course'] + ' ' + i['type'] + ' каб. ' + i['location'] + '\n'
+                messege += '-- ' + str(i['event_index']) + ' ' + i['course'] + ' ' + i['type'] + ' каб. ' + i['location'] + '\n'
             else: 
-                messege += 'занятий нет' + '\n'
+                messege += i + 'занятий нет' + '\n'
 
-    messege1 = 'Неделя: ' + data['schedule'][1]['week'] + '\n'
+    messege1 = '\n' + 'Неделя: ' + data['schedule'][1]['week'] + '\n'
     for elem in data['schedule'][1]['days']:
-        messege1 += elem['day'] + '\n'
+        messege1 += '\n' + elem['day'] + '\n'
         for i in elem['events']:
             if elem['events'].count(i) > 0:
-                messege1 += str(i['event_index']) + ' ' + i['course'] + ' ' + i['type'] + ' каб. ' + i['location'] + '\n'
+                messege1 += '-- ' + str(i['event_index']) + ' ' + i['course'] + ' ' + i['type'] + ' каб. ' + i['location'] + '\n'
             else:
-                messege1 += 'занятий нет' + '\n'
-
+                messege1 += i + 'занятий нет' + '\n'
+    print('-'*31)
+    print_datetime()
+    print('-'*31)
     print(messege)
     print(messege1)
 
-print(nice_print(schedule()))
+def print_res(data):
+    messege = 'Результаты поиска:\n'
+    if len(data) == 1:
+        for elem in data:
+            id = elem['id']
+            if elem['url'][-1] not in 'АБВГДАЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ':
+                s = requests.get(f'https://timetable.magtu.ru/api/v2/groups/{id}/schedule')
+                data = s.json()
+            else:
+                s = requests.get(f'https://timetable.magtu.ru/api/v2/teachers/{id}/schedule')
+                data = s.json()
+            nice_print(data)
+            return data  
+    else: 
+        for elem in data:
+            messege += elem['url'] + '\n'       
+        print(messege)
+        print_res(group_name())
+
+print_res(group_name())
